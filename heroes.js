a056191a959a5168e849f1f502b8a7cb9951c68f
unknown <c_li70655@fanshaweonline.ca>
2019-03-20 19:14:49 -0400

    app2

--- heroes.js
+++ heroes.js
@@ -1,16 +1,22 @@
 const heroes = {
-    name: "Marvel",
-    role: "Marvel Heroes Background Story", 
+    name: "CC Gaming",
+    role: "3A Game For Sale", 
     logo: "logo.png", 
-    m1: "captain.jpg", 
-    m2: "deadpool.jpg", 
-    m3: "hulk.jpg", 
-    m4: "iron.jpg", 
-    m5: "spider.jpg", 
-    m6: "thor.jpg", 
-    m7: "widow.jpg", 
-    m8: "wolf.jpg", 
-    m9: "x.jpg,"
+    m1: "apex-1.png", 
+    m2: "call-1.png", 
+    m3: "dc-1.png", 
+    m4: "dota2-1.jpg", 
+    m5: "lol-1.png", 
+    m6: "mh-1.png", 
+    m7: "pubg-1.jpg", 
+    m8: "sv-1.jpg", 
+    m9: "totalwar-1.png", 
+    free: "Free", 
+    price1: "CAN $79.99", 
+    price2: "CAN $29.99", 
+    price3: "CAN $36.99", 
+    price4: "CAN $16.99", 
+    price5: "CAN $71.99"
 };
 
 module.exports = heroes;
