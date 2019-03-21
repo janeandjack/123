547ce93a67b48fe3339265a1fb579986af6dbae1
unknown <c_li70655@fanshaweonline.ca>
2019-03-21 17:05:52 -0400

    success render data from heroes.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var data = require('../data');
-// var hero = require('../heroes');
+var hero = require('../heroes');
 data.title = `Welcome to Express with Node`;
 
 /* GET home page. */
@@ -11,7 +11,8 @@ data.title = `Welcome to Express with Node`;
 
 router.get('/', function(req, res, next) {
   //console.log('data:', data);
-  res.render('index', {name: "CC Gaming", role: "3A Game For Sale", logo: "logo.png", m1: "apex-1.png", m2: "call-1.png", m3: "dc-1.png", m4: "dota2-1.jpg", m5: "lol-1.png", m6: "mh-1.png", m7: "pubg-1.jpg", m8: "sv-1.jpg", m9: "totalwar-1.png", free: "Free", price1: "CAD $79.99", price2: "CAD $29.99", price3: "CAD $36.99", price4: "CAD $16.99", price5: "CAD $71.99"});
+    res.render('index', hero);
+  res.render('index', {name: "CC Gaming", role: "3A Game For Sale"});
 });
 // 
 router.get('/:user', (req, res, next) => {
